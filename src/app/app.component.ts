5f626a28b2dd86f0c27220f3c758c279ed93f40d
Vishnu Methre <vmethre@manh.com>
2023-06-23 09:54:55 +0530

    todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todolist } from './todolist.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  buttons=['DONE','UPDATE','PEND'];
+  Pendiglist:any=[];
   title = 'vishnuTodolist';
+  constructor(private todo : Todolist){}
+  Todolists:any=this.todo.Todolist;
+  flag=false;
+
+  addTolist(f:any)
+  {
+    this.todo.Todolist.push(f);
+  }
+  remove(index:number)
+  {
+    this.Pendiglist.push(this.Todolists[index]);
+    this.Todolists.splice(index,1);
+  }
+  enable()
+  {
+this.flag=true;
+  }
+  completed(index:number)
+  {
+    this.Todolists.splice(index,1);
+  }
+  deleted(index:number)
+  {
+    this.Pendiglist.splice(index,1);
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Todolist } from './todolist.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [Todolist],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
